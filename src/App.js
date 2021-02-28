90414fbbad7f68b2d7ec22b14399145e835444c0
Guillaume Labussiere <labussieremailpro@gmail.com>
2021-02-28 11:07:35 +0100

    retour aux travail

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router,Switch, Route, Link } from "react-router-dom";
-import Login from "./components/Login";
 import Welcome from "./components/Welcome";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
-      <Router>
-          <div>
-              <nav>
-                  <ul>
-                      <li>
-                          <Link to="/">Home</Link>
-                      </li>
-                      <li>
-                          <Link to="/login">Login</Link>
-                      </li>
-                  </ul>
-              </nav>
-              <Switch>
-                  <Route exact path="/" component={Welcome}/>
-                  <Route path="/login" component={Login}/>
-              </Switch>
-          </div>
-      </Router>
+      <Home/>
     </>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+class Home extends React.Component{
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return (
+            <>
+
+                <nav class="navbar navbar-light bg-light">
+                    <a class="navbar-brand" href="">
+                            <div> Bootstrap </div>
+                    </a>
+                </nav>
+
+            </>
+        )
+    }
+}
+
+export default Home;
