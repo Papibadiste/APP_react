59981f4bf2db23af58b86996daf67e74825616d5
Guillaume Labussiere <labussieremailpro@gmail.com>
2021-03-23 20:19:23 +0100

    fin des action sur le site

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,14 +1,57 @@
 import React from "react"
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Redirect} from "react-router-dom";
+import baseUrl from "../baseurl/BaseUrl";
+import axios from "axios";
 
 
 class Admin extends React.Component{
+
+    url = new baseUrl
+
     constructor() {
         super()
         this.state = {
-
+            pictures: [],
         }
+    }
+
+    componentWillMount() {
+        axios.get(this.url.value + '/api/listrpg')
+            .then(res => {
+                this.setState({pictures: res.data})
+
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
+
+
+    handleSwap = (id) => event  => {
+        console.log(id)
+
+
+        axios.get(this.url.value+'/api/admin/change/'+id)
+            .then(res => {
+                console.log('bien joué')
+                axios.get(this.url.value + '/api/listrpg')
+                    .then(res => {
+                        this.setState({pictures: res.data})
+
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    })
+
+            })
+            .catch(error =>{
+                if(error.response.status === 401){
+                    this.setState({errors : error.response.data.errors} , () => console.log(this.state) )
+                }
+            })
+
     }
 
 
@@ -27,17 +70,24 @@ class Admin extends React.Component{
                         <th scope="col">Nom</th>
                         <th scope="col">Note</th>
                         <th scope="col">description</th>
-                        <th scope="col">fichier</th>
-                        <th scope="col"></th>
+                        <th scope="col">date</th>
+                        <th scope="col">activer</th>
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
+                    {
+                        this.state.pictures.map((picture) =>
+                        <tr>
+                        <th scope="row">{ picture.title}</th>
+                        <td>{ picture.note}/100</td>
+                        <td>{ picture.description}</td>
+                        <td>{ picture.created_at.split("T")[0]}</td>
+                            <td>{ picture.is_active}</td>
+                            <td> <button type="submit" className="btn btn-dark w-100 btninsco" onClick={this.handleSwap(picture.id)}>Swap</button> </td>
+                        </tr>
+                        )
+                    }
+
                     </tbody>
                 </table>
             </>
